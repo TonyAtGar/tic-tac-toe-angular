110177f9496f674015db390a2dd7ecc9031d699f
TonyAtGar <154230055+TonyAtGar@users.noreply.github.com>
2024-03-28 08:16:43 +0000

    d

---
+++ game/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TicTacToeComponent } from './tic-tac-toe/tic-tac-toe.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TicTacToeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
